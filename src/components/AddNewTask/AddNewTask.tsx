8c4503af69aa06f762a5148993385cad4685af37
Sergey Emirzakov <im.seem16@gmail.com>
2022-02-05 11:11:11 +0300

    Refactoring

--- src/components/AddNewTask/AddNewTask.tsx
+++ src/components/AddNewTask/AddNewTask.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
 import { MdAdd } from 'react-icons/md';
 
 interface IAddNewTask {
@@ -9,20 +9,22 @@ interface IAddNewTask {
 export const AddNewTask: React.FC<IAddNewTask> = ({ onShowModalHandler }) => {
   return (
     <>
-      <Box
+      <Button
         onClick={() => onShowModalHandler(true)}
         bg="orange.400"
-        w="40px"
-        h="40px"
+        color="white"
         cursor="pointer"
         display="flex"
         justifyContent="center"
         alignItems="center"
         marginRight="1rem"
-        borderRadius="md">
-        <MdAdd size="1.5rem" color="white" />
-      </Box>
-      <Text fontWeight="medium">Add new task</Text>
+        borderRadius="md"
+        _hover={{ backgroundColor: 'orange.300' }}>
+        <Box marginRight={3}>
+          <MdAdd size="1.5rem" color="white" />
+        </Box>
+        Add new task
+      </Button>
     </>
   );
 };
