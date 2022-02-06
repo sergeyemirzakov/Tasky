79fa5d3891d68168f4f2fabce80c95104ef60485
Sergey Emirzakov <im.seem16@gmail.com>
2022-02-06 21:11:28 +0300

    Refactoring. Added dispatchers

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,71 +1,85 @@
 import React from 'react';
 
-import { Box, Text, Switch } from '@chakra-ui/react';
+import { Box, Text, Switch, Checkbox, Badge } from '@chakra-ui/react';
+import { Tbody, Tr, Td } from '@chakra-ui/react';
+import { RiEdit2Line } from 'react-icons/ri';
 import { GrClose } from 'react-icons/gr';
-import classNames from 'classnames';
+import { MdDeleteOutline } from 'react-icons/md';
 import { CustomTextField } from '../CustomTextField/CustomTextField';
 
 interface TodoItem {
   todo: {
     id: string;
-    important: boolean;
+    priority: string;
     complete: boolean;
     text: string;
   };
   onCompleteTaks: (id: string) => void;
-  onImportantTask: (id: string) => void;
+  // onImportantTask: (id: string) => void;
   removeTask: (id: string) => void;
+  onEditTask: (id: string) => void;
 }
 
 export const TodoItem: React.FC<TodoItem> = ({
   todo,
   onCompleteTaks,
-  onImportantTask,
   removeTask,
+  onEditTask,
 }) => {
+  const [checked, setChecked] = React.useState<boolean>(false);
+  const [showEdit, setShowEdit] = React.useState<boolean>(false);
+
+  const onChangeHandler = (id: string) => {
+    onCompleteTaks(id);
+    setChecked((prev) => !prev);
+  };
+
   return (
     <>
-      <Box
-        className={classNames(
-          'default',
-          { important: todo.important },
-          { complete: todo.complete },
-        )}
-        key={todo.id}
-        p={2}
-        borderRadius="md"
-        min-height="80px">
-        <Box display="flex" justifyContent="space-between" marginBottom={1}>
-          <Box display="flex">
-            <Box display="flex" alignItems="center">
-              <Text fontSize={12} marginRight={1}>
-                Important
-              </Text>
-              <Switch
-                onChange={() => onImportantTask(todo.id)}
-                isChecked={todo.important}
-                size="sm"
-                colorScheme="red"
-                marginRight={4}
-              />
+      <Tbody>
+        <Tr>
+          <Td>
+            <Box>
+              <MdDeleteOutline size="1.2rem" cursor="pointer" />
             </Box>
-            <Box display="flex" alignItems="center">
-              <Text fontSize={12} marginRight={1}>
-                Done
-              </Text>
-              <Switch
-                onChange={() => onCompleteTaks(todo.id)}
-                isChecked={todo.complete}
-                size="sm"
-                colorScheme="green"
-              />
+          </Td>
+          <Td
+            onClick={() => onEditTask(todo.id)}
+            onMouseLeave={() => setShowEdit(false)}
+            onMouseOver={() => setShowEdit(true)}
+            borderLeft="0.5px solid #E2E8F0"
+            cursor="pointer"
+            _hover={{ backgroundColor: 'orange.50' }}>
+            <Box display="flex" alignItems="center" justifyContent="space-between">
+              <Box display="flex">
+                <Checkbox
+                  checked={checked}
+                  onChange={() => onChangeHandler(todo.id)}
+                  colorScheme="green"
+                  marginRight={3}
+                />
+                <Text>{todo.text}</Text>
+              </Box>
+              {showEdit && (
+                <Box display="flex" alignItems="center" cursor="pointer">
+                  <Box>
+                    <RiEdit2Line />
+                  </Box>
+                  <Text fontSize="xs">Edit</Text>
+                </Box>
+              )}
             </Box>
-          </Box>
-          <GrClose onClick={() => removeTask(todo.id)} cursor="pointer" />
-        </Box>
-
-        <CustomTextField id={todo.id} text={todo.text} />
-      </Box>
+          </Td>
+          <Td borderLeft="0.5px solid #E2E8F0">
+            <Badge variant="outline">{todo.priority}</Badge>
+          </Td>
+          <Td borderLeft="0.5px solid #E2E8F0">
+            <Badge colorScheme={!todo.complete ? 'blue' : 'green'} variant="outline">
+              {!todo.complete ? 'In progress' : 'Done'}
+            </Badge>
+          </Td>
+        </Tr>
+      </Tbody>
     </>
   );
 };
