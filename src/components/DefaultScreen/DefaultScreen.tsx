82c2429c14a7b7433d3894e0cc683cb1dcf57ab0
Sergey Emirzakov <im.seem16@gmail.com>
2022-02-10 22:08:34 +0300

    Changed title color

--- src/components/DefaultScreen/DefaultScreen.tsx
+++ src/components/DefaultScreen/DefaultScreen.tsx
@@ -6,7 +6,7 @@ export const DefaultScreen = () => {
   return (
     <>
       <Box height="75vh" display="flex" alignItems="center" justifyContent="center">
-        <Heading as="h1" size="3xl" color="gray.200">
+        <Heading as="h1" size="3xl" color="orange.300">
           No tasks for today :)
         </Heading>
       </Box>
