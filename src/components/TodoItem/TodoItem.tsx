aabd6cf6068dcef1798fce743c247e17ddc0f319
Sergey Emirzakov <im.seem16@gmail.com>
2022-02-25 15:17:47 +0300

    Refactoring

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,89 +1,104 @@
 import React from 'react';
 
-import { Box, Text, Checkbox, Badge } from '@chakra-ui/react';
-import { Tbody, Tr, Td } from '@chakra-ui/react';
-import { RiEdit2Line } from 'react-icons/ri';
+import { Box, Text, Badge, Button } from '@chakra-ui/react';
+import { MdModeEditOutline, MdDragIndicator } from 'react-icons/md';
+import { HiFlag } from 'react-icons/hi';
+import { Todo } from '../../store/taskSlice';
 
 interface TodoItem {
-  todo: {
-    id: string;
-    priority: string;
-    complete: boolean;
-    text: string;
-  };
+  todo: Todo;
   onCompleteTaks: (id: string) => void;
   onEditTask: (id: string, textValue: string, priority: string) => void;
+}
+
+interface Colors {
+  low: string;
+  medium: string;
+  high: string;
 }
 
 export const TodoItem: React.FC<TodoItem> = ({ todo, onCompleteTaks, onEditTask }) => {
   const [checked, setChecked] = React.useState<boolean>(false);
   const [showEdit, setShowEdit] = React.useState<boolean>(false);
 
-  const onChangeHandler = (id: string) => {
+  const onClickHandler = (id: string) => {
     onCompleteTaks(id);
     setChecked((prev) => !prev);
   };
 
+  let proirityColors: Colors = {
+    low: '#D69E2E',
+    medium: '#DD6B20',
+    high: '#E53E3E',
+  };
+
+  let priorityColor = proirityColors[todo.priority.toLowerCase() as keyof Colors];
+
   return (
     <>
-      <Tbody>
-        <Tr>
-          <Td
-            onMouseLeave={() => setShowEdit(false)}
-            onMouseOver={() => setShowEdit(true)}
-            borderLeft="0.5px solid #E2E8F0"
-            cursor="pointer"
-            transition="all .3s"
-            _hover={{ padding: '25px 24px', backgroundColor: 'orange.50' }}>
-            <Box display="flex" alignItems="center" justifyContent="space-between">
-              <Box display="flex">
-                <Checkbox
-                  checked={checked}
-                  onChange={(e) => {
-                    e.stopPropagation();
-                    onChangeHandler(todo.id);
-                  }}
-                  colorScheme="green"
-                  marginRight={3}
-                />
-                <Text>{todo.text}</Text>
-              </Box>
-              {showEdit && (
-                <Box
-                  border="1px"
-                  borderColor="orange.300"
-                  borderRadius="md"
-                  transition="all .3s"
-                  padding="2px 10px"
-                  _hover={{
-                    backgroundColor: 'orange.300',
-                    color: 'white',
-                    padding: '5px 30px',
-                  }}
-                  onClick={(e) => {
-                    onEditTask(todo.id, todo.text, todo.priority);
-                  }}
-                  display="flex"
-                  alignItems="center"
-                  cursor="pointer">
-                  <Box>
-                    <RiEdit2Line />
-                  </Box>
-                  <Text fontSize="xs">Edit</Text>
-                </Box>
-              )}
+      <Box
+        width="310px"
+        minHeight="210px"
+        boxShadow="sm"
+        onMouseLeave={() => setShowEdit(false)}
+        onMouseOver={() => setShowEdit(true)}
+        borderRadius="lg"
+        bg="white"
+        padding="18px 15px"
+        display="inline-flex"
+        flexDirection="column"
+        justifyContent="space-between">
+        <Box>
+          <Box marginBottom="5px" display="flex" justifyContent="space-between">
+            <Text fontSize="18px" fontWeight="bold">
+              {todo.text}
+            </Text>
+            <MdDragIndicator cursor="grab" color="lightgrey" size="1.2rem" />
+          </Box>
+          <Box marginBottom="20px">
+            <Text color="gray.400">{todo.description}</Text>
+          </Box>
+        </Box>
+
+        <Box>
+          <Box display="flex" alignItems="center">
+            <Text color="gray.400" fontSize="14px" marginRight="10px">
+              Priority:
+            </Text>
+            <Box display="flex" alignItems="center">
+              <HiFlag size="1.5rem" color={priorityColor} />
+              <Badge
+                backgroundColor={priorityColor}
+                variant="solid"
+                marginLeft="5px"
+                fontSize="0.8rem">
+                {todo.priority}
+              </Badge>
             </Box>
-          </Td>
-          <Td borderLeft="0.5px solid #E2E8F0">
-            <Badge variant="outline">{todo.priority}</Badge>
-          </Td>
-          <Td borderLeft="0.5px solid #E2E8F0">
-            <Badge colorScheme={!todo.complete ? 'blue' : 'green'} variant="outline">
-              {!todo.complete ? 'In progress' : 'Done'}
-            </Badge>
-          </Td>
-        </Tr>
-      </Tbody>
+          </Box>
+          <Box marginTop="10px" display="flex" justifyContent="space-between">
+            <Button
+              onClick={() => onClickHandler(todo.id)}
+              size="sm"
+              fontSize="0.9rem"
+              colorScheme={!checked ? 'gray' : 'green'}>
+              {!checked ? 'In progress...' : 'It is Done =)'}
+            </Button>
+            {showEdit && (
+              <Button
+                size="sm"
+                onClick={() => {
+                  onEditTask(todo.id, todo.text, todo.priority);
+                }}>
+                <span style={{ display: 'flex' }}>
+                  <MdModeEditOutline />
+                  Edit
+                </span>
+              </Button>
+            )}
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 };
