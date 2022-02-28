5f993f89674dbb72a8a048dfa967fabb1256f663
Sergey Emirzakov <im.seem16@gmail.com>
2022-02-28 23:07:05 +0300

    Changed height

--- src/components/DefaultScreen/DefaultScreen.tsx
+++ src/components/DefaultScreen/DefaultScreen.tsx
@@ -5,7 +5,7 @@ import { Box, Heading } from '@chakra-ui/react';
 export const DefaultScreen = () => {
   return (
     <>
-      <Box height="75vh" display="flex" alignItems="center" justifyContent="center">
+      <Box height="70vh" display="flex" alignItems="center" justifyContent="center">
         <Heading as="h1" size="xl" color="gray.300">
           No tasks for today :)
         </Heading>
